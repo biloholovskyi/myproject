6a5b7db01f7a1a1f3d43383d61aecd4843df404d
biloholovskyi <biloholovskyi@yandex.ru>
2019-09-30 22:27:38 +0300

    babel

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,11 @@ var imagemin = require('gulp-imagemin');
 var minCSS = require('gulp-clean-css');
 var minJS = require('gulp-minify');
 var clean = require('gulp-clean');
-
-
-gulp.task('minJS', () => {
-    gulp.src('./app/js/**/*.js')
-      .pipe(minJS({
-          ext: {
-              src: '-debug.js',
-              min: '.js'
-          },
-          exclude: ['tasks'],
-          ignoreFiles: ['.combo.js', '-min.js']
-      }))
-      .pipe(gulp.dest('./build/js/'))
-
-});
+const babel = require('gulp-babel');
 
 gulp.task('default', function() {
     browserSync.init({
-        proxy: "myproject.site/app"
+        proxy: "myproject/app"
     });
     gulp.watch("app/sass/**/*.scss", function () {
         return gulp.src("app/sass/**/*.scss")
@@ -31,6 +17,14 @@ gulp.task('default', function() {
           .pipe(minCSS())
           .pipe(gulp.dest("app/css"))
           .pipe(browserSync.stream());
+    });
+    gulp.watch("app/js/**/*.js", function () {
+      return gulp.src("app/js/index.js")
+        .pipe(babel({
+          presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest("app/buildjs"))
+        .pipe(browserSync.stream());
     });
     gulp.watch("app/**/*.php").on('change', browserSync.reload);
     gulp.watch("app/**/*.html").on('change', browserSync.reload);
