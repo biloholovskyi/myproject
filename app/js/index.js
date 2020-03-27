2663c66b500032c9070aadff4395c2151127d53d
amitil <biloholovskyi>
2020-03-27 13:23:08 +0200

    slider

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,11 @@
 import 'normalize.css';
+import {nextSlid, prevSlid, calcSlid} from "./slider";
 
 $(document).ready(function(){
-	
+  calcSlid();
+
+	$('.slider__nav .next').on('click', (event) => nextSlid(event));
+	$('.slider__nav .prev').on('click', (event) => prevSlid(event));
 });
 
 $(window).resize(function () {
